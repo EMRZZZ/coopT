501e45eb9e11c765b03d9be75d7c371641625c4e
EMRZZZ <47974077+EMRZZZ@users.noreply.github.com>
2019-09-16 22:16:46 +0800

    add two new funciton that check file and some help information

--- Demo1.py
+++ Demo1.py
@@ -2,16 +2,40 @@ import sys
 import getopt
 import re
 
-arg, opt = getopt.getopt(sys.argv[1:], '-h:-o')
-print(arg[0][1])
-url = arg[0][1]
+opts, args = getopt.getopt(sys.argv[1:], '-u:-o,-f:-o,-h')
+print(opts[0][0])  # opts为二元组的列表，第一个是选项，第二个是其后的参数
+print(opts[0][1])
+opt = opts[0][0]
+url = opts[0][1]
 
 
 def checkURL(url):
-    if re.match('.|<|>|]', url):
+    if re.match('.|<|>|]', url):   # 需要修改，从数据库读取关键词并匹配
         print("ok")
     else:
         print("failed")
 
 
-checkURL(url)
+def checkFile(fileName):
+    with open('./'+fileName, 'r') as f:
+        for line in f.readlines():
+            if re.match('.|<|>|]', line):  
+                print(line)
+            else:
+                continue  # 或者记录下有问题的url并输出文件，再继续下去
+
+
+'''
+file = 'file.txt'
+with open('./'+file, 'r') as f:
+    for line in f.readlines():
+        print(line.strip())
+'''
+
+for o, a in opts:
+    if o in '-u':
+        checkURL(opts[0][1])
+    if o in '-f':
+        checkFile(opts[0][1])
+    if o in '-h':
+        print('URLCheck.py [-u] url, [-f] fileName.txt')
